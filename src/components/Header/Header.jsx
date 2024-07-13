612c4dc5f42a3624816647230ab938404d4541f3
Madhumita2027 <mmsarkar621@gmail.com>
2024-07-13 13:06:07 +0530

    Add Header, Common Button & Input field

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,77 @@
 import React from 'react';
+import { Logo, Container, LogoutBtn } from '../index.js';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
+    const authState = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+            name: 'Home',
+            slug: '/',
+            active: true
+        },
+        {
+            name: 'Login',
+            slug: '/login',
+            active: !authState
+        },
+        {
+            name: 'Signup',
+            slug: '/signup',
+            active: !authState
+        },
+        {
+            name: 'All Posts',
+            slug: '/all-posts',
+            active: authState
+        },
+        {
+            name: 'Add Post',
+            slug: '/add-post',
+            active: authState
+        }
+    ]
+
     return ( 
-        <>
-            <h1>Header</h1>
-        </>
+        <header className='py-3 shadow bg-gray-500'>
+            <Container>
+                <nav className='flex'>
+                    <div className='mr-4'>
+                        <Link to='/'>
+                            <Logo width='70px' />
+                        </Link>
+                    </div>
+
+                    <ul className='flex ml-auto'>
+                        {
+                            navItems.map( (item) => (
+                                item.active ? (
+                                    <li key={item.name}>
+                                        <button
+                                            onClick={navigate(item.slug)}
+                                            className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                        >{item.name}</button>
+                                    </li>
+                                ) : null 
+                            ))
+                        }
+                        {
+                            // if authState is true then () will execute
+                            authState && (
+                                <li>
+                                    <LogoutBtn />
+                                </li>
+                            )
+                        }
+                    </ul>
+                </nav>
+            </Container>
+        </header>
      );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Logo from "./Logo.jsx";
+import Container from "./container/Container.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 
 
 export {
     Header,
     Footer,
+    Logo,
+    Container,
+    LogoutBtn,
 }
